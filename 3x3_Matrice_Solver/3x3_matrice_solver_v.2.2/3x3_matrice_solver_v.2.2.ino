5fb3071cd3ed2641b4ac09d164f6617a5919a3f9
yolo4lifegod <30472599+yolo4lifegod@users.noreply.github.com>
2018-11-30 10:42:19 -0500

    Update 3x3_matrice_solver_v.2.2.ino

--- 3x3_Matrice_Solver/3x3_matrice_solver_v.2.2/3x3_matrice_solver_v.2.2.ino
+++ 3x3_Matrice_Solver/3x3_matrice_solver_v.2.2/3x3_matrice_solver_v.2.2.ino
@@ -4,7 +4,8 @@
  * This project solves a system of three equations with 3  * 
  * variables (x,y,z) and uses Linea Algebra techniques     *
  * Jose Del Valle. Robotics Instructor.                    *
- * Edited by Yash Saxena                                   *
+ * Edited by Yash Saxena.                                  *
+ * Go to lines 34 to 37 to edit matrix.                    *
  * ********************************************************/
  
 // This project uses the library BasicLinearAlgebra.h developed by Tom Stewart (available in the file 3x3_Matrice_Solver)
@@ -28,11 +29,11 @@ void setup() {
     // put your setup code here, to run once:
   Serial.begin(9600);
   
-  // This are example values
+  // These are coefficient values
   A <<  1, -2,  4,
        -1,  4, -4,
         2,  2, -4;
-  
+  // The numbers on the other side of the equation:
   B << 18,-24,-18;
   
   // Now find the Coefficient's matrix inverse 
