ba31db18e4807b2d78c15b9d876bdeae66de7a29
yolo4lifegod <30472599+yolo4lifegod@users.noreply.github.com>
2019-01-29 11:00:17 -0500

    Update Locater_v.2.2.ino

--- Locater/Locater_v.2.2/Locater_v.2.2.ino
+++ Locater/Locater_v.2.2/Locater_v.2.2.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#include <math.h>
 
 #define trigPina 2
 #define echoPina 3
@@ -50,8 +51,11 @@ void loop() {
   m = pow(x,2);
   n = 0-((l-m-(pow(25.5,2))/(51*x)));
   theta = acos(n);
-  a = x * sin(theta*180/pi);
+  l = (theta * 180) / pi;
+  n = double sin(double __l);
+  a = x * n;
   a = a*180/pi;
+  
   l = pow(x,2);
   m = pow(a,2);
   n = l - m;
@@ -61,9 +65,10 @@ void loop() {
   m = pow(e,2);
   n = l + m;
   b = sqrt(n);
+  
   n = a / e;
-  beta = atan(n);
-  beta = beta*180/pi;
+  beta = double atan(double __n);
+  beta = (beta * 180) / pi;
 
   myservo.write(beta);
   delay(500);
@@ -80,40 +85,23 @@ void loop() {
   Serial.println("__________________________");
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////// FUNCTIONS /////////////////////////////////////////////////////////////////////////////////////////////
 
-/* solutions are in radians
-  By- ABHILASH PATEL
-*/
-
-// Create this into lib, give cred
-
-float asin(float c){
-  float out;
-  out= ((c+(c*c*c)/6+(3*c*c*c*c*c)/40+(5*c*c*c*c*c*c*c)/112+
-  (35*c*c*c*c*c*c*c*c*c)/1152 +(c*c*c*c*c*c*c*c*c*c*c*0.022)+
-  (c*c*c*c*c*c*c*c*c*c*c*c*c*.0173)+(c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*.0139)+
-  (c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*0.0115)+(c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*0.01)
-   ));
-
-                                           //asin
-if(c>=.96 && c<.97){out=1.287+(3.82*(c-.96)); }
-if(c>=.97 && c<.98){out=(1.325+4.5*(c-.97));}          // arcsin
-if(c>=.98 && c<.99){out=(1.37+6*(c-.98));}
-if(c>=.99 && c<=1){out=(1.43+14*(c-.99));}  
-return out;}
-
-float acos(float c)
-{float out;
-out=asin(sqrt(1-c*c));
-return out;}
-
-float atan(float c)
-{float out;
-out=asin(c/(sqrt(1+c*c)));
-return out;}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Absolute error <= 6.7e-5
+float acos(float h) {
+  float negate = float(h < 0);
+  h = abs(h);
+  float ret = -0.0187293;
+  ret = ret * h;
+  ret = ret + 0.0742610;
+  ret = ret * h;
+  ret = ret - 0.2121144;
+  ret = ret * h;
+  ret = ret + 1.5707288;
+  ret = ret * sqrt(1.0-h);
+  ret = ret - 2 * negate * ret;
+  return negate * pi + ret;
+}
 
 void scan(){
   long dura, disa, durb, disb;
@@ -122,16 +110,16 @@ void scan(){
   digitalWrite(trigPina, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPina, HIGH);
-  delayMicroseconds(10);
+  delayMicroseconds(100);
   digitalWrite(trigPina, LOW);
   dura = pulseIn(echoPina, HIGH);
-  disa = (dura/2) / 29.1;
+  disa = dura * 330;
   
   // b ultrasonic
   digitalWrite(trigPinb, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPinb, HIGH);
-  delayMicroseconds(10);
+  delayMicroseconds(100);
   digitalWrite(trigPinb, LOW);
   durb = pulseIn(echoPinb, HIGH);
   disb = (durb/2) / 29.1;
