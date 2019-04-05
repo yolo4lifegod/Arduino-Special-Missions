97a066e60ec3ff6c6b08175115a266a5c5d3faeb
yolo4lifegod <30472599+yolo4lifegod@users.noreply.github.com>
2019-04-05 16:35:51 -0400

    Update Smart-Pumping_Ventillation_System.ino

--- Smart-Pumping_Ventillation_System/Smart-Pumping_Ventillation_System.ino
+++ Smart-Pumping_Ventillation_System/Smart-Pumping_Ventillation_System.ino
@@ -4,9 +4,12 @@ dht DHT;
 
 #define waterLevel A0
 
+#define pResistor A1
+
 void setup() {
   Serial.begin(9600);
   pinMode(waterLevel,INPUT);
+  pinMode(pResistor,INPUT);
 }
 
 void loop() {
@@ -17,15 +20,27 @@ void loop() {
   int val = analogRead(waterLevel);
   float w = (val * 100) / 1024;
 
+  int value = analogRead(pResistor);
+  luxmeter(value);
+
   Serial.print("Temperature = ");
   Serial.print(t);
   Serial.println(" *C ");
-  Serial.print("    Humidity = ");
+  Serial.print("Humidity = ");
   Serial.print(h);
   Serial.println(" % ");
   Serial.print("Water Level = ");
   Serial.print(w);
-  Serial.print("%");
+  Serial.println("%");
+  Serial.print("Luxmeters = ");
+  Serial.print(lux);
+  Serial.println(" lux");
+  Serial.println("________________________");
 
   delay(1000);
+}
+
+float luxmeter(a){
+  lux = (1.25 * pow(10,7))*(pow(a,-1.4059))
+  return lux;
 }
