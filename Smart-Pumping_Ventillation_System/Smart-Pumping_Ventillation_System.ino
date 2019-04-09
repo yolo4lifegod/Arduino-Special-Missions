b077d913e025ef179fb8c36de94bc66346f70f6c
yolo4lifegod <30472599+yolo4lifegod@users.noreply.github.com>
2019-04-09 12:43:53 -0400

    Update Smart-Pumping_Ventillation_System.ino

--- Smart-Pumping_Ventillation_System/Smart-Pumping_Ventillation_System.ino
+++ Smart-Pumping_Ventillation_System/Smart-Pumping_Ventillation_System.ino
@@ -1,3 +1,6 @@
+// Graph to find luxmeter: https://www.allaboutcircuits.com/projects/design-a-luxmeter-using-a-light-dependent-resistor/
+
+
 #include <DHT_U.h>
 #define dataPin 8
 dht DHT;
@@ -5,14 +8,23 @@ dht DHT;
 #define waterLevel A0
 
 #define pResistor A1
+/*
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+*/
+#define potPin A2
+
+int applications = 3;
 
 void setup() {
   Serial.begin(9600);
+  // lcd.begin(16,2);
   pinMode(waterLevel,INPUT);
   pinMode(pResistor,INPUT);
 }
 
 void loop() {
+  // Applications
   int readData = DHT.read11(dataPin);
   float t = DHT.temperature;
   float h = DHT.humidity;
@@ -23,6 +35,10 @@ void loop() {
   int value = analogRead(pResistor);
   luxmeter(value);
 
+  // Scrolling screen for LCD
+  int potValue = analogRead(potPin);
+  float scrolldecide = 1024 / (applications - 1);
+  
   Serial.print("Temperature = ");
   Serial.print(t);
   Serial.println(" *C ");
@@ -36,6 +52,12 @@ void loop() {
   Serial.print(lux);
   Serial.println(" lux");
   Serial.println("________________________");
+
+  for(int i = 0; i <= (applications - 1); i++){
+    if(potValue < (i*scrolldecide)){
+      int decider = i
+    }
+  }
 
   delay(1000);
 }
